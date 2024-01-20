da76f1c8fa19c580571034aad10dc718a74dc50a
ShubhamRSharmas <98682917+ShubhamRSharmas@users.noreply.github.com>
2024-01-20 17:02:49 +0530

    Add files via upload

---
+++ Project1.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE EmployeeDB; /*CREATE DATABASE - Creates a database. Syntax: CREATE DATABASE DB_Name*/
+
+USE EmployeeDB; /*USE - Specifies which Database to use. Syntax:USE DB_Name*/
+
+CREATE TABLE employees
+
